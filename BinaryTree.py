f52b9116fb76dd6eb606d4e4e59d2828a356d5d6
vinayaposa <vinaya.g35@gmail.com>
2019-10-04 00:56:54 +0530

    Modified and fixed

--- BinaryTree.py
+++ BinaryTree.py
@@ -15,34 +15,55 @@ class binarytree():
 	def root_node_create(self, value):
 		self.root_node = tree_node(value)
 
-	#Create a leafnode
-	def node_create(self, value):
+    # Add node
+	def add_node(self,value):
 		if self.root_node == None:
-		    return  self.root_node_create(value)
+			self.root_node_create(value)
 		else:
+			self.node_create(value,self.root_node)
+
+
+	#Create a leafnode
+	def node_create(self, value, node):
         # Traversing through the tree to find spot
-        # Assigning the address of self.tree_node to temp
-			temp = self.root_node
-			print(temp)
-	        # looping through the tree
-			while temp:
-				if temp.node_value > value:
-					temp = temp.left_node
-				else:
-					temp = temp.right_node
-				if temp == None:
-					temp = tree_node(value)
-					break
+        # Assigning the address of self.tree_node to temp		
+	    # looping through the tree	
+		print("Adding the node")	
+		if node.node_value > value:
+			if node.left_node is not None:
+				self.node_create(value, node.left_node)
+			else:
+				node.left_node = tree_node(value)
+					
+		else:
+			if node.right_node is not  None:
+				self.node_create(value, node.right_node)
+			else:
+				node.right_node = tree_node(value)
+					
+				
+	#Print Values in tree
+	def print(self):
+		if self.root_node is not None:
+			self.traverse_tree(self.root_node)
 
-
-  
+	#Traverse the tree
+	def traverse_tree(self, node):
+		#Inorder traversal
+		if node is not None:
+			self.traverse_tree(node.left_node)
+			print(node.node_value)
+			self.traverse_tree(node.right_node)
 
 bt = binarytree()
-bt.node_create(10)
-bt.node_create(9)
-bt.node_create(11)
-
-
+bt.add_node(12)
+bt.add_node(9)
+bt.add_node(6)
+bt.add_node(43)
+bt.add_node(10)
+bt.add_node(73)
+bt.add_node(22)
+bt.print()
 
 
 
